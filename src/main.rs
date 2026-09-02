bd3e812553203c8e1d2738b495e2f4b5d60aa84d
MFernflower <machacker9000@gmail.com>
2026-09-02 15:58:10 +0000

    Refactor main function

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
-use framework_lib::chromium_ec::{CrosEc, EcError};
+use framework_lib::chromium_ec::{CrosEc, CrosEcDriver};
 use framework_lib::chromium_ec::commands::RgbS;
-
 const RGBPAYLOAD: [RgbS; 8] = [
     RgbS { r: 0xFF, g: 0xD7, b: 0x00 },
     RgbS { r: 0xFF, g: 0x45, b: 0x00 },
@@ -11,13 +10,10 @@ const RGBPAYLOAD: [RgbS; 8] = [
     RgbS { r: 0xFF, g: 0x63, b: 0x47 },
     RgbS { r: 0xFF, g: 0xD7, b: 0x00 },
 ];
-
-fn main() -> Result<(), EcError> {
+fn main() {
     let ec = CrosEc::new();
-
-    // Set RGB keyboard to the preset color.
+    // Set fan rgb to the preset payload.
     let _ = ec.rgbkbd_set_color(0, RGBPAYLOAD.to_vec());
-
     // Set fan duty cycle to 50% (LE u32).
     let _ = ec.send_command(0x0024u16, 0, &50u32.to_le_bytes());
 }
