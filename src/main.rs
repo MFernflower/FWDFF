440a5c7c737431077c0cc5b5f4a45fd4321eda6a
MFernflower <machacker9000@gmail.com>
2026-09-02 16:01:46 +0000

    Refactor main function to acutally fucking compile

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use framework_lib::chromium_ec::{CrosEc, CrosEcDriver};
 use framework_lib::chromium_ec::commands::RgbS;
+use framework_lib::chromium_ec::EcError;
+
 const RGBPAYLOAD: [RgbS; 8] = [
     RgbS { r: 0xFF, g: 0xD7, b: 0x00 },
     RgbS { r: 0xFF, g: 0x45, b: 0x00 },
@@ -10,10 +12,12 @@ const RGBPAYLOAD: [RgbS; 8] = [
     RgbS { r: 0xFF, g: 0x63, b: 0x47 },
     RgbS { r: 0xFF, g: 0xD7, b: 0x00 },
 ];
-fn main() {
+
+fn main() -> Result<(), EcError> {
     let ec = CrosEc::new();
     // Set fan rgb to the preset payload.
-    let _ = ec.rgbkbd_set_color(0, RGBPAYLOAD.to_vec());
+    ec.rgbkbd_set_color(0, RGBPAYLOAD.to_vec())?;
     // Set fan duty cycle to 50% (LE u32).
-    let _ = ec.send_command(0x0024u16, 0, &50u32.to_le_bytes());
+    ec.send_command(0x0024u16, 0, &50u32.to_le_bytes())?;
+    Ok(())
 }
